fd4b6d4cbbcfe7f3ff6624863dd0935279978161
Sidney <jiangxin87@gmail.com>
2020-04-04 20:59:18 -0400

    fixing build and vendor

--- cmd/walmart-slot-poller.go
+++ cmd/walmart-slot-poller.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"strconv"
 	"time"
-	"github.com/parnurzeal/gorequest"
 )
 
+const (
+	BARHAVEN = "04d2977e-f1be-4d97-86da-a0c3d03c1e8c"
+	BANK     = "ead03854-8104-443a-a958-990332366269"
+)
 
 type slot struct {
 	StartDateTime string
 	EndDateTime   string
 	Status        string
+	SlotId        string
 }
 
 type slotDay struct {
-	Slots []slot
+	Slots         []slot
+	SlotDate      string
+	AccessPointId string
 }
 
 type response struct {
@@ -32,23 +42,43 @@ func sendmail(message string) {
 func checkAvailability(tt time.Time) {
 	today := time.Now()
 	sevenDayLater := today.AddDate(0, 0, 7)
+	eightDayLater := today.AddDate(0, 0, 8)
 	layout := "2006-01-02"
-	t := sevenDayLater.Format(layout)
-	request := gorequest.New()
-	_, body, errs := request.Post("https://www.walmart.ca/api/cart-page/accesspoints/slotavailability?grocery=true").
-		Set("Content-Type", "application/json").
-		Send(`{"startDate": "` + t + `","endDate": "` + t + `","accessPointId": "04d2977e-f1be-4d97-86da-a0c3d03c1e8c","serviceInfo": {"fulfillmentType": "INSTORE_PICKUP"}}`).
-		End()
-	if errs != nil {
-		fmt.Println("error")
+	startDate := sevenDayLater.Format(layout)
+	endDate := eightDayLater.Format(layout)
+	fmt.Printf("checking availibility for %s - %s \n", startDate, endDate)
+	requestBody, err := json.Marshal(map[string]interface{}{
+		"startDate":     startDate,
+		"endDate":       endDate,
+		"accessPointId": BARHAVEN,
+		"serviceInfo": map[string]string{
+			"fulfillmentType": "INSTORE_PICKUP",
+		},
+	})
+	if err != nil {
+		log.Fatalln(err)
 	}
+
+	resp, err := http.Post("https://www.walmart.ca/api/cart-page/accesspoints/slotavailability?grocery=true", "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	res := response{}
-	json.Unmarshal([]byte(body), &res)
-	slotDay := res.SlotDays[0]
-	for _, slot := range slotDay.Slots {
-		if slot.Status == "AVAILABLE" {
-			sendmail(fmt.Sprintf("%s -- %s Slot is %s\n", slot.StartDateTime, slot.EndDateTime, slot.Status))
-			return
+	json.Unmarshal([]byte(string(body)), &res)
+	for _, slotDay := range res.SlotDays {
+		for _, slot := range slotDay.Slots {
+			if slot.Status == "AVAILABLE" {
+				sendmail(fmt.Sprintf("%s -- %s Slot is %s\n", slot.StartDateTime, slot.EndDateTime, slot.Status))
+				return
+			}
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,7 @@ module github.com/SidneyJiang/walmart-slot-poller
 go 1.14
 
 require (
-	github.com/parnurzeal/gorequest v0.2.16
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sendgrid/rest v2.4.1+incompatible // indirect
 	github.com/sendgrid/sendgrid-go v3.5.0+incompatible
-	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
-	moul.io/http2curl v1.0.0 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
 )
